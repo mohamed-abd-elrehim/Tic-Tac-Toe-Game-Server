e22bcb5e3eddf6ffd44d6eb3b6f3100ee45c3a7d
Mohamed Abdelrehim Abdelhady Aly <moabdelraheem19@gmail.com>
2024-07-19 20:51:13 +0300

    add Pakage DTOS , PlayerDTO and Add Two methods to playerDAO (delete and insert)

---
+++ src/DTOS/PlayerDTO.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DTOS;
+
+/**
+ *
+ * @author Mohammed
+ */
+
+
+public class PlayerDTO {
+    int id;
+    int points;
+    String userName;
+    String password;
+    //Status status;
+}
+
