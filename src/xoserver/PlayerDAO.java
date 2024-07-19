99ecbf64e16f1d26d27143088f59cc2a9ab75dfc
Mohamed Abdelrehim Abdelhady Aly <moabdelraheem19@gmail.com>
2024-07-19 20:48:32 +0300

    add Pakage DTOS , PlayerDTO and Add Two methods to playerDAO (delete and insert)

--- src/xoserver/PlayerDAO.java
+++ src/xoserver/PlayerDAO.java
@@ -15,6 +15,7 @@ package xoserver;
  *
  * @author COMPUMARTS
  */
+import DTOS.PlayerDTO;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -44,24 +45,56 @@ public class PlayerDAO {
         }
     }
 
+    
+    
+    
+    
+    static int delete(PlayerDTO p) {
+        try {
+
+            PreparedStatement pst = con.prepareStatement("delete from PLAYER where ID = ?");
+            pst.setInt(1, p.id);
+            result = pst.executeUpdate();
+
+        } catch (SQLException ex) {
+            Logger.getLogger(PlayerDTO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return result;
+    }
+
+    static int insert(PlayerDTO p) {
+        try {
+            //int result;
+            PreparedStatement pst = con.prepareStatement("INSERT INTO PLAYER VALUES (?,?,?,?,?)");
+            //pst.setInt(1, p.id);
+            pst.setString(1, p.userName);
+            pst.setString(2, p.status.toString());
+            pst.setString(3, p.password);
+            pst.setInt(4, p.points);
+
+            result = pst.executeUpdate();
+        } catch (SQLException ex) {
+            Logger.getLogger(PlayerDTO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return result;
+    }
 
 
-//    static int update(PlayerDTO p) {
-//        try {
-//            PreparedStatement pst
-//                    = con.prepareStatement("UPDATE PLAYER SET USERNAME = ?, STATUS = ?, PASSWORD = ?, POINTS = ? WHERE ID = ?");
-//            pst.setString(1, p.userName);
-//            pst.setString(2, p.status.toString());
-//            pst.setString(3, p.password);
-//            pst.setInt(4, p.points);
-//            pst.setInt(5, p.id);
-//            result = pst.executeUpdate();
-//
-//        } catch (SQLException ex) {
-//            Logger.getLogger(PlayerDTO.class.getName()).log(Level.SEVERE, null, ex);
-//        }
-//        return result;
-//    }
+    static int update(PlayerDTO p) {
+        try {
+            PreparedStatement pst = con.prepareStatement("UPDATE PLAYER SET USERNAME = ?, STATUS = ?, PASSWORD = ?, POINTS = ? WHERE ID = ?");
+            pst.setString(1, p.userName);
+            pst.setString(2, p.status.toString());
+            pst.setString(3, p.password);
+            pst.setInt(4, p.points);
+            pst.setInt(5, p.id);
+            result = pst.executeUpdate();
+
+        } catch (SQLException ex) {
+            Logger.getLogger(PlayerDTO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return result;
+    }
 
     
 
